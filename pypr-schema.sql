2d1bdc6d3bcf7d9f9d4dcee65ec646a8bcbf5e31
zhuoyunhui <66240290+zhuoyunhui@users.noreply.github.com>
2021-05-24 12:08:42 +0800

    Add files via upload

---
+++ pypr-schema.sql
@@ -0,0 +1,36 @@
+CREATE SCHEMA `PYPR`;
+USE `PYPR`;
+
+SET NAMES utf8;
+SET character_set_client = utf8mb4;
+
+CREATE TABLE `User` (
+		`userID` VARCHAR(45) UNIQUE NOT NULL,
+	`password` VARCHAR(15) NOT NULL
+			CHECK (LENGTH(`passWord`) >= 6 AND LENGTH(`passWord`) <= 15),
+	`unrealisedBalance` FLOAT NOT NULL 
+			CHECK (`unrealisedBalance` > 0),
+    `availBalance` FLOAT NOT NULL 
+			CHECK (`availBalance` > 0),        
+	PRIMARY KEY(`userID`) 
+);
+    
+CREATE TABLE `Trades` (
+		`tradeID` INT UNIQUE NOT NULL,
+	`userID` VARCHAR(45) UNIQUE NOT NULL,
+	`transactionTime` DATETIME NOT NULL,
+    `ticker` VARCHAR(45) NOT NULL,
+    `quantity` INT NOT NULL
+			CHECK(`quantity` > 0),
+	`currPrice` FLOAT NOT NULL,
+    `entPrice` FLOAT NOT NULL,
+	PRIMARY KEY (`tradeID`)
+);
+
+ALTER TABLE `Trades`
+ADD CONSTRAINT t FOREIGN KEY (`userID`) REFERENCES `User` (`userID`);
+
+
+
+
+    
