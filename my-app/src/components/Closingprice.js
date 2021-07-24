c7bb63efbf9bad283e3db82e6ebef52919d58225
zhuoyunhui <66240290+zhuoyunhui@users.noreply.github.com>
2021-07-24 22:56:21 +0800

    Update Closingprice.js

--- my-app/src/components/Closingprice.js
+++ my-app/src/components/Closingprice.js
@@ -1,5 +1,5 @@
 export async function Closingprice(ticker, yString) {
-    const closingApi = `https://cloud.iexapis.com/stable/stock/${ticker}/chart/date/${yString}?token=pk_0705469d87aa4650835b7e7c86e61296`;
+    const closingApi = `https://cloud.iexapis.com/stable/stock/${ticker}/chart/date/${yString}?token=pk_4421761e7af0413abdd6cf4cc5a9330a`;
 
     const fetcher = await fetch(closingApi);
     const response = await fetcher.json();
