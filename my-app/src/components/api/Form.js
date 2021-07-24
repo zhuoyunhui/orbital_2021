17a8ed6dbdaef10f97a09855e7f2300aeab20f12
sebin <e0552331@u.nus.edu>
2021-07-24 19:10:09 +0800

    Change UI to accept no redirect

--- my-app/src/components/api/Form.js
+++ my-app/src/components/api/Form.js
@@ -1,75 +1,143 @@
-import React, { useState } from "react";
-import { Form, Input, Button } from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Form, Input, Button, Grid, Container } from "semantic-ui-react";
 
-export const TickerForm = ({ onData }) => {
+export const TickerForm = () => {
   const [stock, setStock] = useState("");
   const [day, setDay] = useState();
   const [month, setMonth] = useState();
   const [year, setYear] = useState();
 
-  return (
-    <Form>
-      <Form.Group widths="equal">
-        <Form.Field>
-          <Input
-            placeholder="stock (eg. APPL)"
-            value={stock}
-            onChange={(e) => setStock(e.target.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            placeholder="day (numeric)"
-            value={day}
-            onChange={(e) => setDay(e.target.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            placeholder="month (numeric)"
-            value={month}
-            onChange={(e) => setMonth(e.target.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Input
-            placeholder="year (numeric)"
-            value={year}
-            onChange={(e) => setYear(e.target.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <Button
-            onClick={async () => {
-              const ticker = { stock, day, month, year };
-              // const response = await fetch("/ticker", {
-              //   method: "POST",
-              //   action: "{{ url_for('ticker') }}",
-              //   headers: {
-              //     "Content-Type": "application/json",
-              //   },
-              //   body: JSON.stringify(ticker),
-              // });
-              const response = await fetch("/backtest?ticker=F", {
-                method: "GET",
-              });
+  const [stocks, setStocks] = useState("");
+  // const [date, setDate] = useState();
+  const [sample_size, setSample_size] = useState("");
+  // const [emaused, setEmaused] = useState([]);
+  const [batting_Avg, setBatting_Avg] = useState("");
+  const [gainLossRatio, setGainLossRatio] = useState("");
+  const [averageGain, setAverageGain] = useState("");
+  const [averageLoss, setAverageLoss] = useState("");
+  const [maxReturn, setMaxReturn] = useState("");
+  const [maxLoss, setMaxLoss] = useState("");
+  const [totalReturn, setTotalReturn] = useState("");
+  const [command, setCommand] = useState([]);
 
-              if (response.ok) {
-                console.log("response worked!");
-                console.log(ticker);
-                onData(ticker);
-                setStock("");
-                setDay("");
-                setMonth("");
-                setYear("");
-              }
-            }}
-          >
-            Backtest
-          </Button>
-        </Form.Field>
-      </Form.Group>
-    </Form>
+  return (
+    <>
+      <Form>
+        <Form.Group widths="equal">
+          <Form.Field>
+            <Input
+              placeholder="stock (eg. APPL)"
+              value={stock}
+              onChange={(e) => setStock(e.target.value)}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              placeholder="day (numeric)"
+              value={day}
+              onChange={(e) => setDay(e.target.value)}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              placeholder="month (numeric)"
+              value={month}
+              onChange={(e) => setMonth(e.target.value)}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              placeholder="year (numeric)"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
+          </Form.Field>
+          <Form.Field>
+            <Button
+              onClick={async () => {
+                const ticker = { stock, day, month, year };
+                // const response =
+                fetch("/ticker", {
+                  method: "POST",
+                  action: "{{ url_for('ticker') }}",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(ticker),
+                })
+                  .then((resp) => resp.json())
+                  .then((json) => {
+                    console.log("response worked!");
+                    console.log(ticker);
+                    // onData(ticker);
+                    setStock("");
+                    setDay("");
+                    setMonth("");
+                    setYear("");
+
+                    setStocks(json.stock);
+                    // setDate(data.date);
+                    setSample_size(json.sample_size);
+                    // setEmaused(data.EMAs_used);
+                    setBatting_Avg(json.Batting_Avg);
+                    setGainLossRatio(json.GainLoss_ratio);
+                    setAverageGain(json.Average_Gain);
+                    setAverageLoss(json.Average_Loss);
+                    setMaxReturn(json.Max_Return);
+                    setMaxLoss(json.Max_Loss);
+                    setTotalReturn(json.Total_return);
+                    setCommand(json.Command);
+                  });
+                // const response = await fetch("/backtest?ticker=F", {
+                //   method: "GET",
+                // });
+
+                // if (response.ok) {
+                //   console.log("response worked!");
+                //   console.log(ticker);
+
+                // }
+              }}
+            >
+              Backtest
+            </Button>
+          </Form.Field>
+        </Form.Group>
+      </Form>
+      <Container style={{ marginTop: 40 }}>
+        <Grid
+          divided="vertically"
+          style={{ marginTop: 20, fontFamily: "Quantico" }}
+        >
+          <Grid.Row columns={2} style={{ marginTop: 15 }}>
+            <Grid.Column>
+              <div className="command">
+                <p>
+                  {command.map((cmd) => (
+                    <p>{cmd}, </p>
+                  ))}
+                </p>
+              </div>
+            </Grid.Column>
+            <Grid.Column>
+              <div className="analysis">
+                <p>The stock to backtest: {stocks}.</p>
+                {/* <p>The date is {date}.</p> */}
+                <p>The sample size: {sample_size}.</p>
+                {/* <p>The EMAs used is {emaused}. </p> */}
+                <p>Your Batting Average: {batting_Avg}.</p>
+                <p>Your Gain-Loss Ratio: {gainLossRatio}.</p>
+                <p>Your Average Gain: {averageGain}.</p>
+                <p>Your Average Loss: {averageLoss}.</p>
+                <p>The Maximum Return: {maxReturn}.</p>
+                <p>Your Maximum Loss: {maxLoss}.</p>
+                <p>Your Total Return: {totalReturn}.</p>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
--- my-app/src/components/pages/PageBacktest.js
+++ my-app/src/components/pages/PageBacktest.js
@@ -5,48 +5,48 @@ import { Container, Grid } from "semantic-ui-react";
 import "./PageBacktest.css";
 
 const PageBacktest = () => {
-  const [stocks, setStocks] = useState("");
-  // const [date, setDate] = useState();
-  const [sample_size, setSample_size] = useState("");
-  // const [emaused, setEmaused] = useState([]);
-  const [batting_Avg, setBatting_Avg] = useState("");
-  const [gainLossRatio, setGainLossRatio] = useState("");
-  const [averageGain, setAverageGain] = useState("");
-  const [averageLoss, setAverageLoss] = useState("");
-  const [maxReturn, setMaxReturn] = useState("");
-  const [maxLoss, setMaxLoss] = useState("");
-  const [totalReturn, setTotalReturn] = useState("");
-  const [command, setCommand] = useState([]);
+  // const [stocks, setStocks] = useState("");
+  // // const [date, setDate] = useState();
+  // const [sample_size, setSample_size] = useState("");
+  // // const [emaused, setEmaused] = useState([]);
+  // const [batting_Avg, setBatting_Avg] = useState("");
+  // const [gainLossRatio, setGainLossRatio] = useState("");
+  // const [averageGain, setAverageGain] = useState("");
+  // const [averageLoss, setAverageLoss] = useState("");
+  // const [maxReturn, setMaxReturn] = useState("");
+  // const [maxLoss, setMaxLoss] = useState("");
+  // const [totalReturn, setTotalReturn] = useState("");
+  // const [command, setCommand] = useState([]);
 
   // TickerForm: User input of ticker
-  const [ticker, setTicker] = useState();
-  const [day, setDay] = useState();
-  const [month, setMonth] = useState();
-  const [year, setYear] = useState();
+  // const [ticker, setTicker] = useState();
+  // const [day, setDay] = useState();
+  // const [month, setMonth] = useState();
+  // const [year, setYear] = useState();
 
   // go to backtest.py and get the result
 
-  useEffect(() => {
-    fetch(
-      `https://pypr-backtest.herokuapp.com/backtest?ticker=${ticker}&day=${day}&month=${month}&year=${year}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("useEffect");
-        setStocks(data.stock);
-        // setDate(data.date);
-        setSample_size(data.sample_size);
-        // setEmaused(data.EMAs_used);
-        setBatting_Avg(data.Batting_Avg);
-        setGainLossRatio(data.GainLoss_ratio);
-        setAverageGain(data.Average_Gain);
-        setAverageLoss(data.Average_Loss);
-        setMaxReturn(data.Max_Return);
-        setMaxLoss(data.Max_Loss);
-        setTotalReturn(data.Total_return);
-        setCommand(data.Command);
-      });
-  }, [ticker, day, month, year]);
+  // useEffect(() => {
+  //   fetch(
+  //     `https://pypr-backtest.herokuapp.com/backtest?ticker=${ticker}&day=${day}&month=${month}&year=${year}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log("useEffect");
+  //       setStocks(data.stock);
+  //       // setDate(data.date);
+  //       setSample_size(data.sample_size);
+  //       // setEmaused(data.EMAs_used);
+  //       setBatting_Avg(data.Batting_Avg);
+  //       setGainLossRatio(data.GainLoss_ratio);
+  //       setAverageGain(data.Average_Gain);
+  //       setAverageLoss(data.Average_Loss);
+  //       setMaxReturn(data.Max_Return);
+  //       setMaxLoss(data.Max_Loss);
+  //       setTotalReturn(data.Total_return);
+  //       setCommand(data.Command);
+  //     });
+  // }, [ticker, day, month, year]);
 
   return (
     <>
@@ -77,44 +77,14 @@ const PageBacktest = () => {
         </div>
         <Grid style={{ marginTop: 20 }}>
           <TickerForm
-            onData={(ticker) => {
-              setTicker(ticker["stock"]);
-              setDay(ticker["day"]);
-              setMonth(ticker["month"]);
-              setYear(ticker["year"]);
-            }}
+          // onData={ticker}
+          // {(ticker) => {
+          // setTicker(ticker["stock"]);
+          // setDay(ticker["day"]);
+          // setMonth(ticker["month"]);
+          // setYear(ticker["year"]);
+          // }}
           />
-        </Grid>
-        <Grid
-          divided="vertically"
-          style={{ marginTop: 20, fontFamily: "Quantico" }}
-        >
-          <Grid.Row columns={2} style={{ marginTop: 15 }}>
-            <Grid.Column>
-              <div className="command">
-                <p>
-                  {command.map((cmd) => (
-                    <p>{cmd}, </p>
-                  ))}
-                </p>
-              </div>
-            </Grid.Column>
-            <Grid.Column>
-              <div className="analysis">
-                <p>The stock to backtest: {stocks}.</p>
-                {/* <p>The date is {date}.</p> */}
-                <p>The sample size: {sample_size}.</p>
-                {/* <p>The EMAs used is {emaused}. </p> */}
-                <p>Your Batting Average: {batting_Avg}.</p>
-                <p>Your Gain-Loss Ratio: {gainLossRatio}.</p>
-                <p>Your Average Gain: {averageGain}.</p>
-                <p>Your Average Loss: {averageLoss}.</p>
-                <p>The Maximum Return: {maxReturn}.</p>
-                <p>Your Maximum Loss: {maxLoss}.</p>
-                <p>Your Total Return: {totalReturn}.</p>
-              </div>
-            </Grid.Column>
-          </Grid.Row>
         </Grid>
       </Container>
     </>
