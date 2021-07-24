52b5e2009bc05496715b029e68e5245dee21e8ff
zhuoyunhui <66240290+zhuoyunhui@users.noreply.github.com>
2021-07-24 12:49:26 +0800

    Update app.py

--- my-app/src/components/api/app.py
+++ my-app/src/components/api/app.py
@@ -6,11 +6,6 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
-
 @app.route('/ticker', methods=['POST'])
 @cross_origin()
 def ticker():
